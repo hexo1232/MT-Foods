3013741a87c70cd1b3fdaab3e229f343a2796a49
matias2032 <matiasmatavel1232@gmail.com>
2025-11-20 11:21:30 +0200

    Change default database name from 'railway' to 'restaurante'

--- conexao.php
+++ conexao.php
@@ -4,7 +4,7 @@
 $host = getenv('DB_HOST') ?: "turntable.proxy.rlwy.net";
 $usuario = getenv('DB_USER') ?: "root";
 $password = getenv('DB_PASSWORD') ?: "nnttbKnlsepXHfAwsDOuVOlkkGvngAkN";
-$bd = getenv('DB_NAME') ?: "railway";
+$bd = getenv('DB_NAME') ?: "restaurante";
 $port = getenv('DB_PORT') ?: "56276"; // Atenção: Porta deve ser INT se possível, mas string funciona na maioria
 
 // Ordem correta dos parâmetros: Host, User, Pass, DB, Port
