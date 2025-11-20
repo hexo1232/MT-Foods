0d5a804f230f0303f0d549c6b1e07bb5aea1d32f
matias2032 <matiasmatavel1232@gmail.com>
2025-11-20 12:31:24 +0200

    Update CALLBACK_URL and RETURN_URL to use BASE_DOMAIN

--- API/config_paysuite.php
+++ API/config_paysuite.php
@@ -5,8 +5,8 @@
 define('PAYSUITE_BASE_URL', 'https://paysuite.tech/api/v1');
 define('PAYSUITE_API_TOKEN', '854|S31DEHpO17EaKxOZCwsKi0WVM5aYj26yjRPz12tD3d273e6f'); // substitua pelo seu token real
 define('PAYSUITE_WEBHOOK_SECRET', 'whsec_5eadb165b2a66eab4c64d0ab7f47e9d4c5efd3a787d4d2f2'); // opcional, para validação
-define('CALLBACK_URL', 'https://undebated-man-unrelating.ngrok-free.dev/Restaurante/API/paysuite_callback.php');
-define('RETURN_URL', 'https://undebated-man-unrelating.ngrok-free.dev/Restaurante/finalizar_pedido.php');
+define('CALLBACK_URL', BASE_DOMAIN . '/API/paysuite_callback.php');
+define('RETURN_URL', BASE_DOMAIN . '/finalizar_pedido.php');
 
 
 // Funções auxiliares
