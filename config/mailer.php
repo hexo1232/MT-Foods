15c58a9e25cf14d37f90f875bfbb297de2885dd6
matias2032 <matiasmatavel1232@gmail.com>
2025-11-27 22:52:33 +0200

    Update mailer.php

--- config/mailer.php
+++ config/mailer.php
@@ -8,17 +8,32 @@ use PHPMailer\PHPMailer\Exception;
 function getMailer(): PHPMailer {
     $mail = new PHPMailer(true);
 
-    $mail->isSMTP();
-    $mail->Host       = $_ENV['SMTP_HOST'];
-    $mail->Port       = (int) $_ENV['SMTP_PORT'];
-    $mail->SMTPAuth   = false; // MailHog não requer autenticação
-    $mail->SMTPSecure = false; // Sem TLS/SSL
-
-    $mail->setFrom($_ENV['FROM_EMAIL'], $_ENV['FROM_NAME']);
-    $mail->isHTML(true);
-
-    // DEBUG opcional
-    //$mail->SMTPDebug = 2;
+    try {
+        // Configuração SMTP
+        $mail->isSMTP();
+        $mail->Host       = $_ENV['SMTP_HOST'];
+        $mail->Port       = (int) $_ENV['SMTP_PORT'];
+        $mail->SMTPSecure = $_ENV['SMTP_SECURE']; // 'ssl' ou 'tls'
+        $mail->SMTPAuth   = true; // Gmail PRECISA de autenticação
+        $mail->Username   = $_ENV['SMTP_USER'];
+        $mail->Password   = $_ENV['SMTP_PASS'];
+        
+        // Timeout para evitar travamento
+        $mail->Timeout = 10; // 10 segundos
+        $mail->SMTPKeepAlive = false;
+        
+        // Remetente
+        $mail->setFrom($_ENV['FROM_EMAIL'], $_ENV['FROM_NAME']);
+        $mail->isHTML(true);
+        $mail->CharSet = 'UTF-8';
+        
+        // DEBUG (comentar em produção)
+        // $mail->SMTPDebug = 2; // 0=off, 1=client, 2=client+server
+        
+    } catch (Exception $e) {
+        error_log("Erro ao configurar PHPMailer: " . $e->getMessage());
+        throw $e;
+    }
 
     return $mail;
 }
