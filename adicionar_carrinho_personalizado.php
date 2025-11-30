c81f5240a9f9a86d4de36af30ca39c1bed200e82
matias2032 <matiasmatavel1232@gmail.com>
2025-11-30 12:43:06 +0200

    Update adicionar_carrinho_personalizado.php

--- adicionar_carrinho_personalizado.php
+++ adicionar_carrinho_personalizado.php
@@ -4,6 +4,8 @@ session_start();
 // Inclui o arquivo de conexão com o banco de dados.
 include "conexao.php";
 
+//adicionar_carrinho_personalizado.php, para itens personalizados
+
 // --- 1. RECEBENDO E VALIDANDO OS DADOS DO POST ---
 
 // Recebe e valida os dados básicos do POST.
@@ -217,4 +219,5 @@ if (isset($stmt)) $stmt->close();
 if (isset($stmt_ing)) $stmt_ing->close();
 // Fechar a conexão principal
 if (isset($conexao)) $conexao->close();
+
 ?>
